b785a1445ec97b6b4ce2a158334781420091a133
DenysOstroverkh <Beautifuldevsworld@gmail.com>
2024-11-22 20:23:45 -0800

    mainframe

---
+++ src/components/layout/menu.svelte
@@ -0,0 +1,7 @@
+<nav>
+  <a href="/home"> Home </a>
+  <a href="/about"> About </a>
+  <a href="/portfolio"> Portfolio </a>
+  <a href="/achievement"> Achivement </a>
+  <a href="/contact"> Contact </a>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
   import "../styles/style.scss"; //Import style-sheet
   import { page } from "$app/stores";
-  import Header from "../components/layout/header.svelte";  //Header component
-  import Footer from "../components/layout/footer.svelte";  //Footer component
+  import Header from "../components/layout/header.svelte"; //Header component
+  import Footer from "../components/layout/footer.svelte"; //Footer component
+  import Menu from "../components/layout/menu.svelte"; //Menu component
 
   let uri = $page.url.pathname.toString();
 </script>
 
-<div class="background"></div>  <!-- Redner background photo -->
-<Header></Header> <!-- Render Header component -->
-<main>
-  <slot />  <!-- Dynamic rednering per page -->
-</main>
-<Footer></Footer> <!-- Render Footer component -->
+<!-- Redner background photo -->
+<div class="background"></div>
 
+<!-- Render Header component -->
+<Header />
+<main>
+  <!-- Dynamic rednering per page -->
+  <slot />
+
+  <!-- Render menu -->
+  <Menu />
+</main>
+
+<!-- Render Footer component -->
+<Footer />
