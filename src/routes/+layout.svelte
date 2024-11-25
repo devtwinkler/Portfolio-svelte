6f87c628cfdc87d9ff48fd2ad49ffa55e806687d
DenysOstroverkh <Beautifuldevsworld@gmail.com>
2024-11-24 20:07:37 -0800

    on home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,8 +15,7 @@
   <!-- Dynamic rednering per page -->
   <div
     class="page w-full pt-[1em] pb-[1em]
-      md:pl-[2em] sm:pl-[1em] xs:pl-[0.5em] exs:pl-[0em]
-      md:pr-[1em] sm:pr-[0.5em] xs:pr-[0.2em] exs:pr-[0em]"
+      md:pl-[2em] sm:pl-[1em] xs:pl-[0.5em] exs:pl-[0em]"
   >
     <slot />
   </div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,18 +1,42 @@
 <script>
-  import imgMark01 from "../../assets/imgs/mark-01.png";
+  import imgMark01 from "../../assets/imgs/mark-02.png";
 </script>
 
 <div class="frame">
   <h1>How can you make your life better in the current digital age ?</h1>
 
-  <div>
+  <div class="flex flex-col sm:flex-row gap-[1em] items-center justify-center">
     <img src={imgMark01} alt="..." />
 
     <div>
       <p>
-        Please make your personal website more engaging, attractive,
-        intelligent, unforgettable, and ...
+        🎯 Please make your <b>personal website</b> more engaging, attractive, talented,
+        unforgettable, and ...
       </p>
+      <p>
+        🎯 With cutting-edge technologies, refine your <b>business website</b> to
+        be more welcoming, seo/user-friendly, impressive, and ...
+      </p>
+      <p>
+        🎯 Using <b>modern AI</b>, refine your work more automatic, seamless,
+        intelligent, and ...
+      </p>
+      <p>
+        🎯 Creating a <b>mobile & desktop application</b> if needs, don't miss and
+        get aware of every event related with you, in time
+      </p>
+      <p>
+        🎯 Challenge the opportunity to dive into new and reliable financial
+        networks, like <b>blockchain & smart contract technology</b>
+      </p>
+      <p>
+        🎯 Don't hesitate to <b>commercialize</b> your creative ideas and personal
+        assets, such above software
+      </p>
+      <p class="pt-[0.1em]">
+        Are you interested in embarking on this IT exploration?
+      </p>
+      <p class="high-light">👍 I AM HERE TO ASSIST YOU !</p>
     </div>
   </div>
 </div>
