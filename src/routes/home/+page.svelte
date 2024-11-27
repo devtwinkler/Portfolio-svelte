e2c98685ebc3d38f3584854d202664eed760d97d
DenysOstroverkh <Beautifuldevsworld@gmail.com>
2024-11-26 20:26:15 -0800

    on home

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,5 +1,27 @@
 <script>
   import imgMark01 from "../../assets/imgs/mark-02.png";
+  import iconAI from "../../assets/icons/ai-01.png";
+  import iconWeb from "../../assets/icons/web-01.png";
+  import iconMobile from "../../assets/icons/mobile-01.png";
+  import iconDesktop from "../../assets/icons/desktop-01.png";
+  import iconSmartContract from "../../assets/icons/blockchain-01.png";
+  import iconBlockchain from "../../assets/icons/blockchain-02.png";
+  import iconECommerce from "../../assets/icons/eCommerce-01.png";
+  import iconC from "../../assets/icons/c.png";
+  import iconCpp from "../../assets/icons/c++.png";
+  import iconCShap from "../../assets/icons/cshap.png";
+  import iconPython from "../../assets/icons/python.png";
+  import iconJS from "../../assets/icons/js.png";
+  import iconTS from "../../assets/icons/ts.png";
+  import iconPhp from "../../assets/icons/php.png";
+  import iconDart from "../../assets/icons/dart.png";
+  import iconRust from "../../assets/icons/rust.png";
+  import iconSolidity from "../../assets/icons/solidity.png";
+  import iconRuby from "../../assets/icons/ruby.png";
+  import iconGPT from "../../assets/icons/gpt.png";
+  import iconGoogle from "../../assets/icons/google.png";
+  import iconAWS from "../../assets/icons/aws.png";
+  import iconTensoFlow from "../../assets/icons/TensorFlow.png";
 </script>
 
 <div class="frame">
@@ -8,35 +30,95 @@
   <div class="flex flex-col sm:flex-row gap-[1em] items-center justify-center">
     <img src={imgMark01} alt="..." />
 
-    <div>
+    <div class="text-justify">
       <p>
-        🎯 Please make your <b>personal website</b> more engaging, attractive, talented,
+        🎯 Please make <b>personal website</b> more engaging, attractive, talented,
         unforgettable, and ...
       </p>
       <p>
-        🎯 With cutting-edge technologies, refine your <b>business website</b> to
-        be more welcoming, seo/user-friendly, impressive, and ...
+        🎯 With cutting-edge technologies, refine <b>business service</b> to be more
+        welcoming, impressive, seo/user-friendly, and ...
       </p>
       <p>
-        🎯 Using <b>modern AI</b>, refine your work more automatic, seamless,
+        🎯 Using <b>modern AI</b>, refine every work more automatic, seamless,
         intelligent, and ...
       </p>
       <p>
-        🎯 Creating a <b>mobile & desktop application</b> if needs, don't miss and
-        get aware of every event related with you, in time
+        🎯 Creating <b>mobile & desktop applications</b> as needed, don't miss and
+        get aware of all the events related with you, in time
       </p>
       <p>
         🎯 Challenge the opportunity to dive into new and reliable financial
-        networks, like <b>blockchain & smart contract technology</b>
+        networks, like <b>blockchain & smart contract</b> technology
       </p>
       <p>
         🎯 Don't hesitate to <b>commercialize</b> your creative ideas and personal
         assets, such above software
       </p>
-      <p class="pt-[0.1em]">
-        Are you interested in embarking on this IT exploration?
+      <p class="pt-[0.2em] align-right">
+        <b><i>Are you interested in embarking on this IT exploration?</i></b>
       </p>
-      <p class="high-light">👍 I AM HERE TO ASSIST YOU !</p>
+      <p class="high-light text-right"><i>👍 I AM HERE TO ASSIST YOU !</i></p>
     </div>
+  </div>
+</div>
+
+<div class="frame">
+  <h1>How can I assist you ?</h1>
+
+  <div class="icons to-right animation">
+    <img src={iconBlockchain} alt="..." />
+    <img src={iconSmartContract} alt="..." />
+    <img src={iconDesktop} alt="..." />
+    <img src={iconMobile} alt="..." />
+    <img src={iconWeb} alt="..." />
+    <img src={iconAI} alt="..." />
+    <img src={iconECommerce} alt="..." />
+  </div>
+
+  <div class="text-right">
+    <p class="high-light">
+      <i>I WILL REALIZE YOUR PROJECT, PLAN, IDEA, DREAM, SUCH AS</i> &#11014;
+    </p>
+
+    <p class="high-light">
+      <i>USING LANGUAGES</i> &#11015;
+    </p>
+  </div>
+
+  <div class="icons small to-right">
+    <img src={iconC} alt="..." />
+    <img src={iconCpp} alt="..." />
+    <img src={iconCShap} alt="..." />
+    <img src={iconPython} alt="..." />
+    <img src={iconJS} alt="..." />
+    <img src={iconTS} alt="..." />
+    <img src={iconRust} alt="..." />
+    <img src={iconPhp} alt="..." />
+    <img src={iconDart} alt="..." />
+    <img src={iconSolidity} alt="..." />
+    <img src={iconRuby} alt="..." />
+  </div>
+
+  <div class="icons small">
+    <img src={iconGPT} alt="..." />
+    <img src={iconGoogle} alt="..." />
+    <img src={iconAWS} alt="..." />
+  </div>
+
+  <div>
+    <p class="high-light">
+      &#11014; <i>MANIPULATING SERVICES</i>
+    </p>
+  </div>
+
+  <div>
+    <p class="high-light">
+      &#11015; <i>UTILIZING LIBRARIES / FRAMEWORKS</i>
+    </p>
+  </div>
+
+  <div class="icons small">
+    <img src={iconTensoFlow} alt="..." />
   </div>
 </div>
